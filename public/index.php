983c451cb42acbe922e9d9fa48fef6a79900208f
otaviodev <117609558+otaviofigueredoalves@users.noreply.github.com>
2026-04-17 04:25:15 -0300

    feat: implementado a instância request, jsonresponse e redirectresponse

--- public/index.php
+++ public/index.php
@@ -5,11 +5,12 @@ define('BASE_URL', 'http://localhost:8000');
 require_once __DIR__ . '/../vendor/autoload.php';
 use App\Core\Router;
 use App\Controllers\errors\HttpErrorsController;
+use Symfony\Component\HttpFoundation\Request;
 
 try{
-    $url = $_GET['url'] ?? trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
+    $request = Request::createFromGlobals();
     $router = new Router();
-    $router->dispatch($url);
+    $router->dispatch($request);
 } catch (\Exception $e){
     $error = new HttpErrorsController();
     $error->notServer($e);
