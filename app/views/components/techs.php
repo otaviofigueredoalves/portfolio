71be2a6c8c1c43099daa487ccd9d78e2f0554255
otaviodev <117609558+otaviofigueredoalves@users.noreply.github.com>
2026-07-15 15:18:53 -0300

    torna o carrossel de tecnologias dinâmico

--- app/views/components/techs.php
+++ app/views/components/techs.php
@@ -1,9 +1,9 @@
 <?php
 $count_item = 1;
+$total_techs = count($techs);
 ?>
 <?php foreach($techs as $tech): ?>
-
-    <div class="item item<?= $count_item ?>">
+    <div class="item" style="--quantity: <?= $total_techs ?>; animation-delay: calc(var(--animation-d) / var(--quantity) * (var(--quantity) - <?= $count_item ?>) * -1);">
         <img src="<?= BASE_URL ?>/assets/icons/<?= $tech['icon'] ?>" alt="<?= $tech['nome'] ?>">
     </div>
     <?php $count_item++?>
